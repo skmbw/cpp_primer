8673e4ac16cffc2d905d7332577670864e1abc60
yinlei <tongku2008@126.com>
2020-04-30 16:41:04 +0800

    使用cmake自动生成makefile

--- 1/add.cc
+++ 1/add.cc
@@ -34,7 +34,7 @@ int main()
 	// prompt user to enter two numbers
 	std::cout << "Enter two numbers:" << std::endl; 
 	int v1 = 0, v2 = 0;
-	std::cin >> v1 >> v2;   
+	std::cin >> v1 >> v2;
 	std::cout << "The sum of " << v1 << " and " << v2
 	          << " is " << v1 + v2 << std::endl;
 	return 0;
