3d4a21ed0a0eea266f294cc3a8ea86fd8e7916b3
yinlei <tongku2008@126.com>
2020-05-07 09:25:54 +0800

    练习谓词算法,排序，稳定排序，分区算法

--- 10/exercise_predicate.cpp
+++ 10/exercise_predicate.cpp
@@ -8,19 +8,26 @@
 using namespace std;
 
 bool isShorter(const std::string & a, const std::string & b) {
-    return a.size() > b.size();
+    return a.size() >= b.size();
 }
 
-void elimDups(std::vector<std::string> &words) {
-    std::stable_sort(words.begin(), words.end(), isShorter);
+void elimDups(std::vector<std::string> & words) {
+//    std::sort(words.begin(), words.end(), isShorter); // 这里不能使用谓词，否则就不是默认的字典序了，会有重复的the
+    std::sort(words.begin(), words.end());
 
     // 返回指向最后一个不重复元素后的迭代器
-    auto ret = std::unique(words.begin(), words.end());
-    words.erase(ret, words.end());
+    auto ret_iterator = std::unique(words.begin(), words.end());
+    words.erase(ret_iterator, words.end());
+    // 使用迭代器的普通for循环
+    for(auto b = words.begin(); b != words.end(); ++b) {
+        std::cout << *b << " ";
+    }
+    std::cout << std::endl; // 刷新缓存
 }
 
 int main() {
     vector<string> wordList;
+    wordList.emplace_back("bigger");
     wordList.emplace_back("the");
     wordList.emplace_back("red");
     wordList.emplace_back("red");
@@ -30,8 +37,24 @@ int main() {
     wordList.emplace_back("ok");
     wordList.emplace_back("ok");
     wordList.emplace_back("yin");
+    wordList.emplace_back("exercise");
 
+    // 按字典排序，并删除重复的
     elimDups(wordList);
+    // 按长度大小排序，不按字典序
+    std::stable_sort(wordList.begin(), wordList.end(), isShorter);
+    // 这个迭代根使用迭代器一样，这个更简介。foreach 循环
+    for(auto & b : wordList) {
+        std::cout << b << " ";
+    }
+
+    std::cout << std::endl;
+
+    auto before_iterator = std::partition(wordList.begin(), wordList.end(), [](std::string & s)-> bool {return (s.length() > 4);});
+    for (auto b = wordList.begin(); b != before_iterator; ++b) {
+        std::cout << *b << " ";
+    }
+    std::cout << std::endl;
 
     return 0;
 }
