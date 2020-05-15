4e4f28052789fd43c01f3f7e55ff14dd7ee1c9a7
skmbw <tongku2008@126.com>
2020-05-15 10:32:00 +0800

    注释代码

--- 15/Query.cc
+++ 15/Query.cc
@@ -89,11 +89,14 @@ AndQuery::eval(const TextQuery& text) const
     // virtual calls through the Query operands to get result sets for the operands
     QueryResult left = lhs.eval(text), right = rhs.eval(text);
 
+    // 使用make_shared也是可以的，分配一个空的set<line_no>
+//    shared_ptr<set<line_no>> results = std::make_shared<set<line_no>>();
 	// set to hold the intersection of left and right
     shared_ptr<set<line_no> > ret_lines(new set<line_no>);  
 
     // writes the intersection of two ranges to a destination iterator
     // destination iterator in this call adds elements to ret
+    // 计算两个集合的交集，将结果插入到结果集中
     set_intersection(left.begin(), left.end(), 
                    right.begin(), right.end(),
                    inserter(*ret_lines, ret_lines->begin()));
