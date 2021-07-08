2059cad380d30b13db30b58e6cc4a50f4b48bbb7
yinlei <tongku2008@126.com>
2021-07-08 13:28:50 +0800

    处理curl返回的数据

--- learn_boost/learn_boost.cpp
+++ learn_boost/learn_boost.cpp
@@ -4,6 +4,22 @@
 
 #include "learn_boost.h"
 #include <iostream>
+#include <string>
+
+/**
+ * curl返回数据的回调函数
+ * @param ptr
+ * @param size 每次都是1？size * mem 表示返回的数据长度
+ * @param mem 每次返回的内容
+ * @param stream 数据流
+ * @return
+ */
+size_t write_data(void *ptr, size_t size, size_t mem, void *stream) {
+    auto psResponse = (std::string*) stream; // 强制转换
+//    psResponse->append((char*) ptr, size * mem); // size * mem 表示接受数据的多少
+    std::cout << *psResponse << std::endl;
+    return size * mem;
+}
 
 int main() {
     learn_boost learnBoost;
@@ -14,9 +30,12 @@ int main() {
 //    auto curl = curl_easy_init();
     CURL* curl = curl_easy_init(); // 获得curl的句柄
     curl_easy_setopt(curl, CURLOPT_URL, "https://www.baidu.com");
+    std::string str;
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+//    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &str);
     auto ipInfo = curl_easy_getinfo(curl, CURLINFO_LOCAL_IP, 0);
     auto result = curl_easy_perform(curl);
-
+//    std::cout << str << std::endl;
     curl_easy_cleanup(curl); // 清理，释放资源
     return 0;
 }
